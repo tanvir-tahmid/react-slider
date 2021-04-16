0a09cb9898ed0a63bd7764e29710d50f67016786
Pavilion <tanv.mhn@gmail.com>
2021-04-16 17:41:41 +0600

    initial

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://images.unsplash.com/photo-1559548331-f9cb98001426?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    name: 'John Doe',
+    title: 'Manager',
+    quote:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 2,
+    image:
+      'https://images.unsplash.com/photo-1571844307880-751c6d86f3f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1117&q=80',
+    name: 'Jane Doe',
+    title: 'Designer',
+    quote:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 3,
+    image:
+      'https://images.unsplash.com/photo-1598387746216-506f6bd47aad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    name: 'David Doe',
+    title: 'Developer',
+    quote:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 4,
+    image:
+      'https://images.unsplash.com/photo-1608504174559-b9eeee51c568?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    name: 'Susana Doe',
+    title: 'Product Owner',
+    quote:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
+
+export default people;
